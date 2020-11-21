459522c17ae74c7e7d2a75f36e2283d85bf80f42
rouine <rouinebarry@gmail.com>
2020-11-21 20:11:16 +0100

    fix: resolve bug initilization #1

---
+++ apolline-flutter/lib/bluetoothDevicesPage.dart
@@ -0,0 +1,201 @@
+import 'package:apollineflutter/sensor.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class BluetoothDevicesPage extends StatefulWidget {
+  BluetoothDevicesPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  bool bluetoothIsOn = false;
+
+  @override
+  _BluetoothDevicesPageState createState() => _BluetoothDevicesPageState();
+}
+
+class _BluetoothDevicesPageState extends State<BluetoothDevicesPage> {
+  String state = "Scanning...";
+  bool timeout = true;
+  Map<String, BluetoothDevice> devices = {};
+  Map<String, BluetoothDevice> pairedDevices = {};
+  List<BluetoothDevice> alreadyUsedDevice = [];
+  
+  @override
+  void initState() {
+    super.initState();
+     _performDetection();
+  }
+
+  /* Starts BLE detection */
+  void _performDetection() {
+    
+    // Start scanning
+    setState(() {
+      timeout = false;
+    });
+    
+    widget.flutterBlue.startScan(timeout: Duration(seconds: 10)).then((val) {
+      setState(() {
+        timeout = true;
+      });
+    });
+
+    widget.flutterBlue.connectedDevices.asStream().listen((List<BluetoothDevice> ds) {
+      for (BluetoothDevice device in ds) {
+        setState(() {
+          pairedDevices.putIfAbsent(device.id.toString(), () => device);
+        });
+      }
+    });
+    /* For each result, insert into the detected devices list if not already present */
+    var subscription = widget.flutterBlue.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        setState(() {
+          devices.putIfAbsent(r.device.id.toString(), () => r.device);
+        });
+      }
+    });
+
+    /*setState(() {
+      state = "Detected devices:";
+    });*/
+  }
+
+  void _addWidgetDevices(Map<String, BluetoothDevice> devices, List<Widget> l, Function(List<Widget>, BluetoothDevice) cond) {
+    devices.forEach((id, d) {
+      if (cond(l, d))
+        l.add(Card(
+            child: ListTile(
+            title: Text(d.name),
+            subtitle: Text(id),
+            onTap: () {
+              connectToDevice(d, id);
+            },
+        )));
+    });
+  }
+
+  bool _conditionForDevices(List<Widget> l, BluetoothDevice d) {
+    return (!pairedDevices.containsValue(d)) && (!l.contains(d));
+  }
+
+  bool _conditionForPaireddevices(List<Widget> l, BluetoothDevice d) {
+    return !l.contains(d);
+  }
+
+  /* Build the UI list of detected devices */
+  List<Widget> _buildDevicesList() {
+    List<Widget> wList = new List<Widget>();
+    /* Add the state label at the top */
+    //wList.add(Text(state)); // TODO: remove
+    if(pairedDevices.length > 0) {
+      wList.add(Text("Périphérique appairés"));
+      _addWidgetDevices(pairedDevices, wList, _conditionForPaireddevices);
+    }
+
+    wList.add(Text("Appareils disponibles"));
+    _addWidgetDevices(devices, wList, _conditionForDevices);
+    /* Add a button for each device */
+    /* TODO: filter device list */
+    
+
+    return wList;
+  }
+
+  /* Handles a click on a device entry */
+  void connectToDevice(BluetoothDevice device, String id) async {
+    /* Stop scanning, if not already stopped */
+    FlutterBlue.instance.stopScan();
+    /* We selected a device - go to the device screen passing information about the selected device */
+    var isconnected = await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (context) => SensorView(device: device)),
+    );
+
+    if(isconnected) {
+      setState(() {
+        devices.remove(id);
+        pairedDevices.putIfAbsent(id, () => device);
+      });
+    }
+  }
+
+  ///
+  ///Exécuter lorsqu'on clique sur le button Annalyser ou Arreter
+  void _onPressLookforButton() {
+    if(timeout == true) {
+      _performDetection();
+    } else {
+      widget.flutterBlue.stopScan();
+    }
+  }
+
+  List<Widget> _buildChildrenButton() {
+    if(timeout) {
+      return <Widget>[
+        Text("Analyser"),
+      ];
+    } else {
+      return <Widget>[
+        SizedBox(
+          child: CircularProgressIndicator(backgroundColor: Colors.blue), //TODO choisir une meilleur couleur
+          width: 20,
+          height: 20,
+        ),
+        Text("Arreter"),
+      ];
+    }
+  }
+
+  List<Widget> _buildAppBarAction() {
+    List<Widget> wList = <Widget>[
+      FlatButton(
+        onPressed: () {
+          _onPressLookforButton();
+        },
+        child: Row(
+          children: _buildChildrenButton()
+        ),
+      ),
+    ];
+    return wList;
+  }
+
+  /* UI update only */
+  @override
+  Widget build(BuildContext context) {
+    /* Scan for BLE devices (should be once) */
+    //_performDetection();
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+        actions: _buildAppBarAction(),
+      ),
+      body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: ListView(children: _buildDevicesList())
+      ),
+    );
+  }
+}
--- apolline-flutter/lib/sensor.dart
+++ apolline-flutter/lib/sensor.dart
@@ -1,16 +1,17 @@
-import 'dart:collection';
+import 'dart:async';
 
 import 'package:apollineflutter/gattsample.dart';
 import 'package:apollineflutter/sensormodel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+
 
 class SensorView extends StatefulWidget {
   SensorView({Key key, this.device}) : super(key: key);
 
   final BluetoothDevice device;
+  bool isConnected = false;
 
   @override
   State<StatefulWidget> createState() => _SensorViewState();
@@ -20,8 +21,8 @@ class _SensorViewState extends State<SensorView> {
   String state = "Connecting to the device...";
   String buf = "";
   SensorModel lastReceivedData;
-
   bool initialized = false;
+  StreamSubscription sub; //used for remove listening value to sensor
 
   /* Called when data is received from the sensor */
   void _handleCharacteristicUpdate(List<int> value)
@@ -30,23 +31,21 @@ class _SensorViewState extends State<SensorView> {
     buf += s;
 
     if(buf.contains('\n'))
-      {
-        print("Got full line: " + buf);
-        List<String> values = buf.split(';');
-        /* Split values in a parseable format, and send them to the UI */
-        setState(() {
-          lastReceivedData = SensorModel(values: values);
-          initialized = true;
+    {
+      print("Got full line: " + buf);
+      List<String> values = buf.split(';');
+      /* Split values in a parseable format, and send them to the UI */
+      setState(() {
+        lastReceivedData = SensorModel(values: values);
+        initialized = true;
 
-          /* Perform additional handling here */
-
-        });
-        buf = "";
-      }
+        /* Perform additional handling here */
+      });
+      buf = "";
+    }
   }
 
-  void updateState(String st)
-  {
+  void updateState(String st) {
     print(st);
     setState(() {
       state = st;
@@ -59,6 +58,7 @@ class _SensorViewState extends State<SensorView> {
     {
       updateState("Blue Sensor Dust Sensor found - configuring characteristic");
       var characteristics = service.characteristics;
+
       /* Search for the Dust Sensor characteristic */
       for(BluetoothCharacteristic c in characteristics) {
         if(c.uuid.toString().toLowerCase() == BlueSensorAttributes.DustSensorCharacteristicUUID)
@@ -67,20 +67,20 @@ class _SensorViewState extends State<SensorView> {
 
           /* Enable notification */
           updateState("Enable notification");
-
-          /* Enable notification */
+          
           c.setNotifyValue(true).then((s) {
-
             /* Catch updates on characteristic  */
-            c.value.listen((value) {
+            
+            sub = c.value.listen((value) {
               _handleCharacteristicUpdate(value);
-            }
-            );
+            });
 
             /* Now we tell the sensor to start sending data by sending char 'c' (?) */
-            updateState("Starting up streaming");
-            c.write([0x63]).then((s) {
-              print("Requested streaming start");
+            Timer(Duration(seconds: 3), () {
+              //updateState("Starting up streaming");
+              c.write([0x63]).then((s) {
+                print("Requested streaming start");
+              });
             });
           });
         }
@@ -104,26 +104,32 @@ class _SensorViewState extends State<SensorView> {
   Future<void> initializeDevice()
   async {
     print("Connecting to device");
-    await widget.device.connect();
 
-    /* Listen on connection state changes */
-    var deviceStateSub = widget.device.state.listen((s) {
-      if(s == BluetoothDeviceState.connected)
-        {
-          /* Perform device initialization */
-          handleDeviceConnect(widget.device);
-        } else {
-        /* TODO: Handle reconnection here */
+    try {
+      await widget.device.connect();
+      widget.isConnected = true;
+    } catch(e) {
+      if(e.code != "already_connected") {
+        throw e;
       }
-    });
-
-
+      if(e.code == "already_connected") {
+        widget.isConnected = true;
+      }
+    } finally {
+      handleDeviceConnect(widget.device);
+    }
   }
 
   @override
   void initState() {
     super.initState();
     initializeDevice();
+  }
+
+  @override
+  void dispose() {
+    sub?.cancel();
+    super.dispose();
   }
 
   /* UI update only */
@@ -134,6 +140,12 @@ class _SensorViewState extends State<SensorView> {
       return Scaffold(
         appBar: AppBar(
           title: Text(widget.device.name),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back), 
+            onPressed: () {
+              Navigator.pop(context, widget.isConnected);
+            }
+          ),
         ),
         body: Center(
           child: Column(
@@ -150,6 +162,12 @@ class _SensorViewState extends State<SensorView> {
       return Scaffold(
         appBar: AppBar(
           title: Text(widget.device.name),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back), 
+            onPressed: () {
+              Navigator.pop(context, true);
+            }
+            ),
         ),
         body: Center(
           child: Column(
