b3a54df875e6c15b89cef335feab72277b8a9b2e
Barry Issagha <44679171+issaghaBarry@users.noreply.github.com>
2021-01-20 12:57:20 +0100

    Feat/data transmission on influx db (#25)
    * fix: optimization when send data
    
    Co-authored-by: Gdissa Ramy <ramy.gdissa.etu@univ-lille.fr>

---
+++ apolline-flutter/lib/models/sensor_collection.dart
@@ -0,0 +1,32 @@
+import 'package:apollineflutter/sensormodel.dart';
+
+class SensorCollection {
+
+  List<SensorModel> lastData;
+
+  int get length {
+    return this.lastData.length;
+  }
+
+  SensorCollection() {
+    this.lastData = [];
+  }
+
+  void addModel(SensorModel model) {
+    this.lastData.add(model);
+  }
+
+  void clear() {
+    this.lastData.clear();
+  }
+
+  String fmtToInfluxData() {
+    var result = "";
+
+    for(var i = 0; i < this.lastData.length; i++ ) {
+      result += "${this.lastData[i].fmtToInfluxData()}\n";
+    }
+    return result;
+  }
+
+}
--- apolline-flutter/lib/sensor.dart
+++ apolline-flutter/lib/sensor.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:apollineflutter/models/sensor_device.dart';
 import 'package:apollineflutter/services/influxdb_client.dart';
+import 'package:apollineflutter/models/sensor_collection.dart';
 
 import 'services/realtime_data_service.dart';
 import 'services/service_locator.dart';
@@ -36,7 +37,7 @@ class _SensorViewState extends State<SensorView> {
   StreamSubscription subBluetoothState; //used for remove listening value to sensor
   StreamSubscription subLocation;
   bool isConnected = false;
-  List<SensorModel> lastData = [];
+  SensorCollection lastData = SensorCollection();
 
   List<StreamSubscription> subs =
       []; //used for remove listening value to sensor
@@ -61,7 +62,6 @@ class _SensorViewState extends State<SensorView> {
       var position = this._currentPosition ?? Position();
       
       var model = SensorModel(values: values, device: SensorDevice(widget.device), position: position);
-      _service.write(model.fmtToInfluxData());
       _dataService.update(values);
       this.updateOrWriteData(model);
       
@@ -77,12 +77,10 @@ class _SensorViewState extends State<SensorView> {
 
   void updateOrWriteData(SensorModel model) {
     if(this.lastData.length >= 60) {
-      this.lastData.forEach((pModel) {
-        _service.write(pModel.fmtToInfluxData());
-      });
+      _service.write(this.lastData.fmtToInfluxData());
       this.lastData.clear();
     } else {
-      this.lastData.add(model);
+      this.lastData.addModel(model);
     }
   }
 
