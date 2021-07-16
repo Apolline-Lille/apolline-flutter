1470096388df030ac54db4b2e1b33f1e7915b867
Remy Raes <raes.remy@gmail.com>
2021-07-16 15:04:35 +0200

    [chore] using environment variables in configuration generation script

--- scripts/configuration_generation.dart
+++ scripts/configuration_generation.dart
@@ -4,11 +4,11 @@ Future<void> main() async {
   final filename = 'apolline-flutter/assets/cfg/config_dev.json';
   File(filename).writeAsString(
       """{
-    "api_url": "http://192.168.0.33",
-    "ping_url": "http://192.168.0.33/health",
-    "password": "apollineapp",
-    "username": "apollineapp",
-    "dbname": "apolline",
+    "api_url": "${Platform.environment['APOLLINE_API_URL']}",
+    "ping_url": "${Platform.environment['APOLLINE_API_URL']}/health",
+    "password": "${Platform.environment['APOLLINE_PASSWORD']}",
+    "username": "${Platform.environment['APOLLINE_USERNAME']}",
+    "dbname": "${Platform.environment['APOLLINE_DBNAME']}",
     "minPmValues": [1, 2, 3, 4, 5, 6, 7, 8, 9],
     "maxPmValues": [9, 10, 11, 12, 13, 14, 15, 16, 17]
 }"""
