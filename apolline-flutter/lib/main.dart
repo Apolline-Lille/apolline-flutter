833b672c2feacbe61df2351b0591db560df3ea89
Remy Raes <contact@remyraes.com>
2021-08-27 18:56:22 +0200

    [chore] adding translation files architecture

--- apolline-flutter/lib/main.dart
+++ apolline-flutter/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
 
@@ -8,7 +9,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GlobalConfiguration().loadFromPath("assets/config_dev.json");
   setupServiceLocator();
-  runApp(ApollineApp());
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en', 'GB'), Locale('fr', 'FR')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en', 'GB'),
+        child: ApollineApp()
+    ),
+  );
 }
 // This acts as the landing window of the app.
 // Scans and displays Bluetooth devices in range, and allows to connect to them.
@@ -18,12 +28,15 @@ class ApollineApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Apolline',
       theme: ThemeData(
         primaryColor: Colors.green,
         floatingActionButtonTheme: FloatingActionButtonThemeData(backgroundColor: Colors.lightGreen)
       ),
-      home: BluetoothDevicesPage(title: 'Apolline - Sensors'),
+      home: BluetoothDevicesPage(title: "devicesView.title".tr()),
     );
   }
 }
