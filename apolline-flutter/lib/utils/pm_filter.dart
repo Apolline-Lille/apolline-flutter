8ec57817a488cea8b0ba36a136e3140f7b623a96
Rémy Raes <contact@remyraes.com>
2021-09-01 16:23:25 +0200

    [fix] filters data classes are now private (cannot be used outside packages)

--- apolline-flutter/lib/utils/pm_filter.dart
+++ apolline-flutter/lib/utils/pm_filter.dart
@@ -23,23 +23,23 @@ enum PMFilter {
   PM_ABOVE_10
 }
 
-class PMFilterValues {
+class _PMFilterValues {
   final String label;
   final int dataRowIndex;
-  PMFilterValues({@required this.label, @required this.dataRowIndex});
+  _PMFilterValues({@required this.label, @required this.dataRowIndex});
 }
 
 extension PMFilterUtils on PMFilter {
-  static final Map<PMFilter, PMFilterValues> _values = {
-    PMFilter.PM_1: PMFilterValues(label: "PM_1", dataRowIndex: 1),
-    PMFilter.PM_2_5: PMFilterValues(label: "PM_2_5", dataRowIndex: 2),
-    PMFilter.PM_10: PMFilterValues(label: "PM_10", dataRowIndex: 3),
-    PMFilter.PM_ABOVE_0_3: PMFilterValues(label: "PM_ABOVE_0_3", dataRowIndex: 4),
-    PMFilter.PM_ABOVE_0_5: PMFilterValues(label: "PM_ABOVE_0_5", dataRowIndex: 5),
-    PMFilter.PM_ABOVE_1: PMFilterValues(label: "PM_ABOVE_1", dataRowIndex: 6),
-    PMFilter.PM_ABOVE_2_5: PMFilterValues(label: "PM_ABOVE_2_5", dataRowIndex: 7),
-    PMFilter.PM_ABOVE_5: PMFilterValues(label: "PM_ABOVE_5", dataRowIndex: 8),
-    PMFilter.PM_ABOVE_10: PMFilterValues(label: "PM_ABOVE_10", dataRowIndex: 9)
+  static final Map<PMFilter, _PMFilterValues> _values = {
+    PMFilter.PM_1: _PMFilterValues(label: "PM_1", dataRowIndex: 1),
+    PMFilter.PM_2_5: _PMFilterValues(label: "PM_2_5", dataRowIndex: 2),
+    PMFilter.PM_10: _PMFilterValues(label: "PM_10", dataRowIndex: 3),
+    PMFilter.PM_ABOVE_0_3: _PMFilterValues(label: "PM_ABOVE_0_3", dataRowIndex: 4),
+    PMFilter.PM_ABOVE_0_5: _PMFilterValues(label: "PM_ABOVE_0_5", dataRowIndex: 5),
+    PMFilter.PM_ABOVE_1: _PMFilterValues(label: "PM_ABOVE_1", dataRowIndex: 6),
+    PMFilter.PM_ABOVE_2_5: _PMFilterValues(label: "PM_ABOVE_2_5", dataRowIndex: 7),
+    PMFilter.PM_ABOVE_5: _PMFilterValues(label: "PM_ABOVE_5", dataRowIndex: 8),
+    PMFilter.PM_ABOVE_10: _PMFilterValues(label: "PM_ABOVE_10", dataRowIndex: 9)
   };
 
   int getRowIndex () {
--- apolline-flutter/lib/utils/time_filter.dart
+++ apolline-flutter/lib/utils/time_filter.dart
@@ -19,28 +19,28 @@ enum TimeFilter {
   THIS_WEEK,
 }
 
-class TimeFilterValues {
+class _TimeFilterValues {
   String label;
   int Function() toMinutes;
-  TimeFilterValues({@required this.label, @required this.toMinutes});
+  _TimeFilterValues({@required this.label, @required this.toMinutes});
 }
 
 extension TimeFilterUtils on TimeFilter {
-  static final Map<TimeFilter, TimeFilterValues> _values = {
-    TimeFilter.LAST_MIN: TimeFilterValues(label: "last minute", toMinutes: () => 1),
-    TimeFilter.LAST_5_MIN: TimeFilterValues(label: "last 5 minutes", toMinutes: () => 5),
-    TimeFilter.LAST_15_MIN: TimeFilterValues(label: "last 15 minutes", toMinutes: () => 15),
-    TimeFilter.LAST_30_MIN: TimeFilterValues(label: "last 30 minutes", toMinutes: () => 30),
-    TimeFilter.LAST_HOUR: TimeFilterValues(label: "last 1 hour", toMinutes: () => 60),
-    TimeFilter.LAST_3_HOURS: TimeFilterValues(label: "last 3 hours", toMinutes: () => 180),
-    TimeFilter.LAST_6_HOURS: TimeFilterValues(label: "last 6 hours", toMinutes: () => 360),
-    TimeFilter.LAST_12_HOURS: TimeFilterValues(label: "last 12 hours", toMinutes: () => 720),
-    TimeFilter.LAST_24_HOURS: TimeFilterValues(label: "last 24 hours", toMinutes: () => 1440),
-    TimeFilter.TODAY: TimeFilterValues(label: "today", toMinutes: () {
+  static final Map<TimeFilter, _TimeFilterValues> _values = {
+    TimeFilter.LAST_MIN: _TimeFilterValues(label: "last minute", toMinutes: () => 1),
+    TimeFilter.LAST_5_MIN: _TimeFilterValues(label: "last 5 minutes", toMinutes: () => 5),
+    TimeFilter.LAST_15_MIN: _TimeFilterValues(label: "last 15 minutes", toMinutes: () => 15),
+    TimeFilter.LAST_30_MIN: _TimeFilterValues(label: "last 30 minutes", toMinutes: () => 30),
+    TimeFilter.LAST_HOUR: _TimeFilterValues(label: "last 1 hour", toMinutes: () => 60),
+    TimeFilter.LAST_3_HOURS: _TimeFilterValues(label: "last 3 hours", toMinutes: () => 180),
+    TimeFilter.LAST_6_HOURS: _TimeFilterValues(label: "last 6 hours", toMinutes: () => 360),
+    TimeFilter.LAST_12_HOURS: _TimeFilterValues(label: "last 12 hours", toMinutes: () => 720),
+    TimeFilter.LAST_24_HOURS: _TimeFilterValues(label: "last 24 hours", toMinutes: () => 1440),
+    TimeFilter.TODAY: _TimeFilterValues(label: "today", toMinutes: () {
         DateTime now = DateTime.now();
         return now.hour*60 + now.minute;
       }),
-    TimeFilter.THIS_WEEK: TimeFilterValues(label: "This week",
+    TimeFilter.THIS_WEEK: _TimeFilterValues(label: "This week",
       toMinutes: () {
         DateTime now = DateTime.now();
         int minutesForToday = now.hour*60 + now.minute;
