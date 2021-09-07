c58e86b84d84ef35e1952a6d6cf4fcd7a2c590c7
Rémy Raes <contact@remyraes.com>
2021-09-07 16:04:29 +0200

    [feat] PM thresholds are stored as user preferences

--- apolline-flutter/lib/models/user_configuration.dart
+++ apolline-flutter/lib/models/user_configuration.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:apollineflutter/utils/pm_filter.dart';
 import 'package:apollineflutter/utils/time_filter.dart';
 
@@ -10,31 +12,54 @@ class UserConfiguration {
   TimeFilter _timeFilter;
   ///which PM data to display
   PMFilter _pmFilter;
+  ///pm size concentration limits
+  Map<PMFilter, List<int>> _thresholdsValues;
 
   ///Json keys
   static const String TIME_FILTER_KEY = "timeFilterValue";
   static const String PM_FILTER_KEY = "pmFilterValue";
+  static const String THRESHOLDS_KEY = "thresholdsValue";
 
   ///
   ///Constructor
-  UserConfiguration({timeFilter: TimeFilter.LAST_MIN, pmFilter: PMFilter.PM_2_5}) {
+  UserConfiguration({timeFilter: TimeFilter.LAST_MIN, pmFilter: PMFilter.PM_2_5, Map<PMFilter, int> thresholds}) {
     this._timeFilter = timeFilter;
     this._pmFilter = pmFilter;
+    this._thresholdsValues = {
+      PMFilter.PM_1: [10, 25]
+    };
   }
 
   ///
   ///Constructor from json
-  UserConfiguration.fromJson(Map json) {
-    this._timeFilter = TimeFilter.values[json[UserConfiguration.TIME_FILTER_KEY]];
-    this._pmFilter = PMFilter.values[json[UserConfiguration.PM_FILTER_KEY]];
+  UserConfiguration.fromJson(Map jsonMap) {
+    this._timeFilter = TimeFilter.values[jsonMap[UserConfiguration.TIME_FILTER_KEY]];
+    this._pmFilter = PMFilter.values[jsonMap[UserConfiguration.PM_FILTER_KEY]];
+
+    Map<String, dynamic> values = json.decode(jsonMap[UserConfiguration.THRESHOLDS_KEY]);
+    Map<PMFilter, List<int>> thresholds = Map();
+    print(values);
+    values.forEach((key, value) {
+      thresholds.putIfAbsent(PMFilter.values[int.parse(key)], () => value.cast<int>());
+    });
+
+    this._thresholdsValues = thresholds;
+    print(this._thresholdsValues);
   }
 
   ///
   ///formate class to json.
   Map<String, dynamic> toJson() {
+    var jsonValues = {};
+    this._thresholdsValues.keys.forEach((element) {
+      jsonValues[element.index.toString()] = this._thresholdsValues[element];
+    });
+    print(json.encode(jsonValues));
+
     return {
       UserConfiguration.TIME_FILTER_KEY: this.timeFilter.index,
-      UserConfiguration.PM_FILTER_KEY: this._pmFilter.index
+      UserConfiguration.PM_FILTER_KEY: this._pmFilter.index,
+      UserConfiguration.THRESHOLDS_KEY: json.encode(jsonValues)
     };
   }
 
