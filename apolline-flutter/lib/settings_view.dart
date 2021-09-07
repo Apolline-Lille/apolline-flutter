4479e5f6516f64301c6707f20b22bdc8e1b066fb
Rémy Raes <contact@remyraes.com>
2021-09-07 13:44:35 +0200

    [fix] adjusting cards padding

--- apolline-flutter/lib/settings_view.dart
+++ apolline-flutter/lib/settings_view.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class SettingsPanel extends StatefulWidget {
-  final EdgeInsets padding = EdgeInsets.symmetric(horizontal: 20, vertical: 10);
+  final EdgeInsets padding = EdgeInsets.only(left: 20, right: 20, top: 30, bottom: 10);
   final List<TextInputFormatter> formatters =
     [TextInputFormatter.withFunction((oldValue, newValue) => newValue.text.length > 3 ? oldValue : newValue)];
 
@@ -16,47 +16,48 @@ class SettingsPanel extends StatefulWidget {
 class _SettingsPanelState extends State<SettingsPanel> {
   Widget _buildPMCard (PMFilter indicator) {
     return Card(
-        child: Wrap(
-          children: [
-            Container(
-                padding: EdgeInsets.only(left: 15, top: 10, bottom: 20),
-                child: Text(indicator.getLabel().tr(), style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold
-                ))
-            ),
-            ListTile(
-              title: Text("Warning threshold"),
-              trailing: Container(
-                width: 80,
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  inputFormatters: widget.formatters,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "15",
-                      suffixIcon: Text("µm/m³")
-                  ),
+      margin: EdgeInsets.only(bottom: 20),
+      child: Wrap(
+        children: [
+          Container(
+              padding: EdgeInsets.only(left: 15, top: 10, bottom: 20),
+              child: Text(indicator.getLabel().tr(), style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold
+              ))
+          ),
+          ListTile(
+            title: Text("Warning threshold"),
+            trailing: Container(
+              width: 80,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: widget.formatters,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "15",
+                    suffixIcon: Text("µm/m³")
                 ),
               ),
             ),
-            ListTile(
-              title: Text("Danger threshold"),
-              trailing: Container(
-                width: 80,
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  inputFormatters: widget.formatters,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "30",
-                      suffixIcon: Text("µm/m³")
-                  ),
+          ),
+          ListTile(
+            title: Text("Danger threshold"),
+            trailing: Container(
+              width: 80,
+              child: TextField(
+                keyboardType: TextInputType.number,
+                inputFormatters: widget.formatters,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "30",
+                    suffixIcon: Text("µm/m³")
                 ),
               ),
-            )
-          ],
-        )
+            ),
+          )
+        ],
+      )
     );
   }
 
@@ -67,9 +68,9 @@ class _SettingsPanelState extends State<SettingsPanel> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: widget.padding,
       child: ListView(
         children: _buildAllPMCards(),
+        padding: widget.padding
       ),
     );
   }
