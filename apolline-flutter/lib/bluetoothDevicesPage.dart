cb453aa593b964b1010687f2c59e35ae2e0aa0a7
Rémy Raes <contact@remyraes.com>
2021-09-14 15:25:28 +0200

    [feat] adding a padding around devices view

--- apolline-flutter/lib/bluetoothDevicesPage.dart
+++ apolline-flutter/lib/bluetoothDevicesPage.dart
@@ -277,7 +277,7 @@ class _BluetoothDevicesPageState extends State<BluetoothDevicesPage> {
           child: Container(
             child: ListView(
               children: _buildDevicesList(),
-              padding: EdgeInsets.all(10)
+              padding: EdgeInsets.all(17)
             )
           )
       ),
