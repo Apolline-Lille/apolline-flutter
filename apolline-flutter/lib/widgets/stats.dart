55966d428a871d7a6dbc1891bbc3f39cd53359af
Rémy Raes <contact@remyraes.com>
2021-09-20 11:10:31 +0200

    [refactor] removing useless code

--- apolline-flutter/lib/widgets/stats.dart
+++ apolline-flutter/lib/widgets/stats.dart
@@ -7,20 +7,6 @@ import 'package:apollineflutter/services/service_locator.dart';
 import 'package:apollineflutter/utils/pm_filter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:mp_chart/mp/chart/line_chart.dart';
-import 'package:mp_chart/mp/controller/line_chart_controller.dart';
-import 'package:mp_chart/mp/core/common_interfaces.dart';
-import 'package:mp_chart/mp/core/data/line_data.dart';
-import 'package:mp_chart/mp/core/data_interfaces/i_line_data_set.dart';
-import 'package:mp_chart/mp/core/data_set/line_data_set.dart';
-import 'package:mp_chart/mp/core/description.dart';
-import 'package:mp_chart/mp/core/entry/entry.dart';
-import 'package:mp_chart/mp/core/enums/axis_dependency.dart';
-import 'package:mp_chart/mp/core/enums/x_axis_position.dart';
-import 'package:mp_chart/mp/core/highlight/highlight.dart';
-import 'package:mp_chart/mp/core/utils/color_utils.dart';
-import 'package:mp_chart/mp/core/value_formatter/value_formatter.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 
@@ -33,28 +19,13 @@ class Stats extends StatefulWidget {
   }
 }
 
-class StatsState extends State<Stats> implements OnChartValueSelectedListener {
+class StatsState extends State<Stats> {
   StatsState({Key key});
   RealtimeDataService _dataService = locator<RealtimeDataService>();
-  // stream to listen the arrival of new data 
   Stream<DataPointModel> _dataStream = locator<RealtimeDataService>().dataStream;
-  // a controller with the stream it controls.
   StreamSubscription<DataPointModel> _streamSubscription;
   List<DataPointModel> _data;
-  // contorller for chart
-  LineChartController controller;
-  // Line PM1
-  ILineDataSet setPM1;
-  // Line PM2.5
-  ILineDataSet setPM2_5;
-  // Line PM10
-  ILineDataSet setPM10;
-  bool intialized = false;
-  double i0 = 0;
-  double i1 = 0;
-  double i2 = 0;
-  // used to get format date
-  List<String> _dataTimeX = [];
+
   @override
   void dispose() {
     _streamSubscription.cancel();
@@ -64,55 +35,18 @@ class StatsState extends State<Stats> implements OnChartValueSelectedListener {
   @override
   void initState() {
     this._data = [];
-
-    // _initController();
-    // add point to chart after recieve newData
     _streamSubscription = _dataStream.listen((newData) {
       setState(() => _data.add(newData));
-
-      if (intialized) {
-        _addEntry(
-            0, i0++, double.parse(newData.values[DataPointModel.SENSOR_PM_1]));
-        _addEntry(
-            1, i1++, double.parse(newData.values[DataPointModel.SENSOR_PM_2_5]));
-        _addEntry(
-            2, i2++, double.parse(newData.values[DataPointModel.SENSOR_PM_10]));
-        _dataTimeX.add(newData.values[DataPointModel.SENSOR_DATE]);
-        setState(() {});
-      }
     });
     super.initState();
-    //create line for PM1, PM2.5 and PM10
-    Timer(Duration(milliseconds: 0), () {
-      LineData data = controller?.data;
-      data = LineData();
-      controller.data = data;
-      setPM1 = _createSet("PM1", Colors.black);
-      data.addDataSet(setPM1);
-      setPM2_5 = _createSet("PM2.5", Colors.red);
-      data.addDataSet(setPM2_5);
-      setPM10 = _createSet("PM10", Colors.yellow);
-      data.addDataSet(setPM10);
-      intialized = true;
-    });
-  }
-
-  // button to stop or play chart
-  void _togglePulsar() {
-    if (_dataService.isRunning) {
-      _dataService.stop();
-    } else {
-      _dataService.start();
-    }
-    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: _getChart(), // LineChart(controller),
+        body: _getChart(),
         floatingActionButton: FloatingActionButton(
-          onPressed: _togglePulsar,
+          onPressed: () => _dataService.isRunning ? _dataService.stop() : _dataService.start(),
           child: Icon(
               _dataService.isRunning ? Icons.pause : Icons.play_arrow
           ),
@@ -128,8 +62,6 @@ class StatsState extends State<Stats> implements OnChartValueSelectedListener {
         title: ChartTitle(text: 'Pollution exposition'),
         // Enable legend
         legend: Legend(isVisible: true),
-        // Enable tooltip
-        // tooltipBehavior: _tooltipBehavior,
 
         series: <LineSeries<DataPointModel, String>>[
           LineSeries<DataPointModel, String>(
@@ -158,97 +90,5 @@ class StatsState extends State<Stats> implements OnChartValueSelectedListener {
           )
         ]
     );
-  }
-  
-  //init chart
-  void _initController() {
-    var desc = Description()..enabled = false;
-    controller = LineChartController(
-        axisLeftSettingFunction: (axisLeft, controller) {
-          axisLeft
-            ..drawGridLines = (false)
-            ..setAxisMinimum(0);
-        },
-        axisRightSettingFunction: (axisRight, controller) {
-          axisRight
-            ..drawGridLines = (false)
-            ..setAxisMinimum(0);
-        },
-        legendSettingFunction: (legend, controller) {
-          legend
-            ..wordWrapEnabled = (true)
-            ..drawInside = (false);
-        },
-        xAxisSettingFunction: (xAxis, controller) {
-          xAxis
-            ..position = (XAxisPosition.BOTH_SIDED)
-            ..setAxisMinimum(0)
-            ..setGranularity(1)
-            ..setValueFormatter(A(_dataTimeX))
-            ..setAxisMaximum(
-                controller.data == null ? 0 : controller.data.xMax + 0.25);
-        },
-        noDataText:
-             "statsView.noData".tr(),
-        drawGridBackground: false,
-        dragXEnabled: true,
-        dragYEnabled: true,
-        scaleXEnabled: true,
-        scaleYEnabled: true,
-        selectionListener: this,
-        pinchZoomEnabled: true,
-        description: desc);
-  }
-
-  @override
-  void onNothingSelected() {}
-
-  @override
-  void onValueSelected(Entry e, Highlight h) {}
-
-  final List<Color> colors = ColorUtils.VORDIPLOM_COLORS;
-
-  // add point (x, y) for line which the index is setIndex
-  void _addEntry(int setIndex, double x, double y) {
-    LineData data = controller?.data;
-
-    if (data == null) {
-      data = LineData();
-      controller.data = data;
-    }
-    data.addEntry(Entry(x: x, y: y), setIndex);
-    data.notifyDataChanged();
-
-    controller.setVisibleXRangeMaximum(6);
-    controller.moveViewTo(
-        (data.getEntryCount() - 7).toDouble(), 50, AxisDependency.LEFT);
-    controller.state?.setStateIfNotDispose();
-  }
-
-  // create line data for PM1, PM10, PM2.5
-  LineDataSet _createSet(String name, Color color) {
-    LineDataSet set = LineDataSet(null, name);
-    set.setLineWidth(2.5);
-    set.setCircleRadius(4.5);
-    set.setColor1(color);
-    set.setCircleColor(Color.fromARGB(255, 240, 99, 99));
-    set.setHighLightColor(Color.fromARGB(255, 190, 190, 190));
-    set.setAxisDependency(AxisDependency.LEFT);
-    set.setValueTextSize(10);
-    return set;
-  }
-}
-
-// used to format date
-class A extends ValueFormatter {
-  A(this._dataTimeX) : super();
-
-  final List<String> _dataTimeX;
-  // get format date
-  @override
-  String getFormattedValue1(double value) {
-    List<String> timeX =
-        _dataTimeX[value.toInt() % _dataTimeX.length].split('_');
-    return timeX[3] + ':' + timeX[4] + ':' + timeX[5];
   }
 }
