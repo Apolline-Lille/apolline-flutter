e8dd17306d7ceaa5c797a8bdc3b0d406b2552f29
Rémy Raes <contact@remyraes.com>
2021-09-30 11:41:43 +0200

    [test] computer should return battery level of 25%

--- apolline-flutter/test/battery.dart
+++ apolline-flutter/test/battery.dart
@@ -20,6 +20,12 @@ void main () {
     expect(percentageResult, 100);
   });
 
+  test('should return battery level of 25%', () {
+    const currentVoltage = 3.725;  // between 3.7V (20%) and 3.75V (30%) steps
+    final int percentageResult = getBatteryPercentageFromVoltageValue(currentVoltage);
+    expect(percentageResult, 25);
+  });
+
   test('should return battery level of 95%', () {
     const currentVoltage = 4.15;  // between 4.1V (90%) and 4.2V (100%) steps
     final int percentageResult = getBatteryPercentageFromVoltageValue(currentVoltage);
