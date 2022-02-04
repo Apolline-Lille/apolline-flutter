40e21097db5dac7e4729db5c3934befe61bde607
RyanIkhlef <ryan.ikhlef.etu@univ-lille.fr>
2022-02-04 16:05:38 +0100

    [feat] Exception thrown when neither the influxdb password nor the token are provide

---
+++ apolline-flutter/lib/exception/server_credentials_exception.dart
@@ -0,0 +1,8 @@
+import 'package:apollineflutter/exception/apolline_abstract_exception.dart';
+
+
+class ServerCredentialsException extends ApollineAbstractException {
+
+  ServerCredentialsException(String msg) : super(msg);
+
+}
