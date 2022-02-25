71611ba78b883974ff292e86209094dd96dcbfc6
RyanIkhlef <ryan.ikhlef.etu@univ-lille.fr>
2022-02-25 15:04:31 +0100

    [fix] imports issue

--- apolline-flutter/lib/widgets/periodic_quality.dart
+++ apolline-flutter/lib/widgets/periodic_quality.dart
@@ -1,7 +1,7 @@
 import 'package:apollineflutter/models/data_point_model.dart';
 import 'package:apollineflutter/models/user_configuration.dart';
 import 'package:apollineflutter/widgets/charts/BatteryLevelIndicator.dart';
-import 'package:easy_localization/src/public_ext.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
