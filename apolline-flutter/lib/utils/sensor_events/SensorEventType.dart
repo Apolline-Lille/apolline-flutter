a3e25c56bcf9c9f57ef44999db4b90458de750e4
Rémy Raes <contact@remyraes.com>
2022-12-14 11:09:01 +0100

    feat: add colors for DataSendingOk and DataSendingFail events

--- apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
+++ apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
@@ -17,8 +17,8 @@ extension SensorEventTypeUtils on SensorEventType {
     SensorEventType.Connection: _SensorEventTypeData("events.types.connection", Colors.green.shade100),
     SensorEventType.Disconnection: _SensorEventTypeData("events.types.disconnection", Colors.red.shade100),
     SensorEventType.LiveData: _SensorEventTypeData("events.types.liveData", Colors.white),
-    SensorEventType.DataSendingOk: _SensorEventTypeData("events.types.dataSendingOk", Colors.white),
-    SensorEventType.DataSendingFail: _SensorEventTypeData("events.types.dataSendingFailed", Colors.white)
+    SensorEventType.DataSendingOk: _SensorEventTypeData("events.types.dataSendingOk", Colors.blue.shade50),
+    SensorEventType.DataSendingFail: _SensorEventTypeData("events.types.dataSendingFailed", Colors.red.shade100)
   };
 
   String get label {
