0f850f5d0e27fe36ee77985a1e95e2e837989088
Rémy Raes <contact@remyraes.com>
2022-12-14 10:56:41 +0100

    refactor: rewrite SensorEventTypeUtils so it can contain more data

--- apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
+++ apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
@@ -1,22 +1,29 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 
 enum SensorEventType {
   Connection, Disconnection, LiveData,
   DataSendingOk, DataSendingFail
 }
 
+class _SensorEventTypeData {
+  final String label;
+  final Color color;
+  _SensorEventTypeData(this.label, this.color);
+}
+
 extension SensorEventTypeUtils on SensorEventType {
-  static final Map<SensorEventType, String> _labels = {
-    SensorEventType.Connection: "events.types.connection",
-    SensorEventType.Disconnection: "events.types.disconnection",
-    SensorEventType.LiveData: "events.types.liveData",
-    SensorEventType.DataSendingOk: "events.types.dataSendingOk",
-    SensorEventType.DataSendingFail: "events.types.dataSendingFailed"
+  static final Map<SensorEventType, _SensorEventTypeData> _labels = {
+    SensorEventType.Connection: _SensorEventTypeData("events.types.connection", Colors.green.shade100),
+    SensorEventType.Disconnection: _SensorEventTypeData("events.types.disconnection", Colors.red.shade100),
+    SensorEventType.LiveData: _SensorEventTypeData("events.types.liveData", Colors.white),
+    SensorEventType.DataSendingOk: _SensorEventTypeData("events.types.dataSendingOk", Colors.white),
+    SensorEventType.DataSendingFail: _SensorEventTypeData("events.types.dataSendingFailed", Colors.white)
   };
 
   String get label {
     if (SensorEventTypeUtils._labels[this] == null)
       throw RangeError("This SensorEventType has no associated label.");
-    return SensorEventTypeUtils._labels[this]!.tr();
+    return SensorEventTypeUtils._labels[this]!.label.tr();
   }
 }
