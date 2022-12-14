efa38b4b36d5cdc4aec2e86e0f088c83114d64f7
Rémy Raes <contact@remyraes.com>
2022-12-14 11:21:30 +0100

    refactor: Events dialog (#34)
    * feat: make live data cards smaller
    
    * refactor: put all events dialog code into a dedicated widget
    
    * feat: don't show live data events by default
    
    * refactor: EventsDialog requires an events list as input
    
    * refactor: dynamically build event tiles
    
    * feat: a button allows to switch live data events display
    
    * fix: adjust component state
    
    * feat: register data sending events
    
    When data is sent to the server, application checks the response
    status code, and logs an event accordingly (either "data sent
    successfully" or "failed sending data").
    
    * fix: event tiles color does not go over dialog title anymore
    
    * refactor: rewrite SensorEventTypeUtils so it can contain more data
    
    * refactor: sensor event types have an associated color
    
    * feat: add colors for DataSendingOk and DataSendingFail events

--- apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
+++ apolline-flutter/lib/utils/sensor_events/SensorEventType.dart
@@ -1,19 +1,35 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 
 enum SensorEventType {
-  Connection, Disconnection, LiveData
+  Connection, Disconnection, LiveData,
+  DataSendingOk, DataSendingFail
+}
+
+class _SensorEventTypeData {
+  final String label;
+  final Color color;
+  _SensorEventTypeData(this.label, this.color);
 }
 
 extension SensorEventTypeUtils on SensorEventType {
-  static final Map<SensorEventType, String> _labels = {
-    SensorEventType.Connection: "events.types.connection",
-    SensorEventType.Disconnection: "events.types.disconnection",
-    SensorEventType.LiveData: "events.types.liveData"
+  static final Map<SensorEventType, _SensorEventTypeData> _labels = {
+    SensorEventType.Connection: _SensorEventTypeData("events.types.connection", Colors.green.shade100),
+    SensorEventType.Disconnection: _SensorEventTypeData("events.types.disconnection", Colors.red.shade100),
+    SensorEventType.LiveData: _SensorEventTypeData("events.types.liveData", Colors.white),
+    SensorEventType.DataSendingOk: _SensorEventTypeData("events.types.dataSendingOk", Colors.blue.shade50),
+    SensorEventType.DataSendingFail: _SensorEventTypeData("events.types.dataSendingFailed", Colors.red.shade100)
   };
 
   String get label {
     if (SensorEventTypeUtils._labels[this] == null)
-      throw RangeError("This SensorEventType has no associated label.");
-    return SensorEventTypeUtils._labels[this]!.tr();
+      throw RangeError("This SensorEventType has no associated data.");
+    return SensorEventTypeUtils._labels[this]!.label.tr();
+  }
+
+  Color get color {
+    if (SensorEventTypeUtils._labels[this] == null)
+      throw RangeError("This SensorEventType has no associated data.");
+    return SensorEventTypeUtils._labels[this]!.color;
   }
 }
