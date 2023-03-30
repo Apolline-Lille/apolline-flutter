26851944b73a2e93e9803bff3d42943b70ffe6c5
Rémy Raes <contact@remyraes.com>
2023-03-30 18:26:33 +0200

    feat: April updates (#35)
    * build: bump version to 1.8.3
    * feat: AM2320 values (#36)
    * refactor: prepare AM2320 data integration
    * refactor: export sensor support check in a dedicated method
    * feat: send AM2320 data to backend if available
    * refactor: make hasAM2320Sensor method public
    * test: add a few hasAM2320Sensor tests
    * refactor: rename hasAM2320Sensor method to featuresAM2320SensorData

--- apolline-flutter/lib/models/data_point_model.dart
+++ apolline-flutter/lib/models/data_point_model.dart
@@ -122,19 +122,28 @@ class DataPointModel {
     var tmpK = addNestedData("temperature.k", this.temperatureK.toString(), Units.TEMPERATURE_KELVIN);
     var tmpDps310 = addNestedData("temperature_dps310.c", this.values[SENSOR_TEMP_DPS310], Units.TEMPERATURE_CELSIUS);
     var tmpAdj = addNestedData("temperature_adjusted.c", this.values[SENSOR_TEMP_ADJUSTED], Units.TEMPERATURE_CELSIUS);
-    // var tmpAm2320 = addNestedData("temperature_am2320.c", this.values[SENSOR_TEMP_AM2320], Units.TEMPERATURE_CELSIUS);
 
     // Humidity
     var humi = addNestedData("humidity", this.values[SENSOR_HUMI], Units.PERCENTAGE);
     var humiC = addNestedData("humidity.compensated", this.humidityC.toString(), Units.PERCENTAGE);
     var humAdj = addNestedData("humidity_adjusted", this.values[SENSOR_HUMI_ADJUSTED], Units.PERCENTAGE);
-    // var humAm2320 = addNestedData("humidity_am2320", this.values[SENSOR_HUMI_AM2320], Units.PERCENTAGE);
 
+    // Others
     var batLevel = addNestedData("voltage", this.values[SENSOR_VOLT], Units.VOLTAGE);
     var pressure = addNestedData("pressure", this.values[SENSOR_PRESSURE], Units.PRESSURE);
 
-    // TODO add AM2320 metrics once sensor has actually been added
-    return "$pm1\n$pm25\n$pm10\n$pm03ab\n$pm05ab\n$pm1ab\n$pm25ab\n$pm5ab\n$pm10ab\n$tmpC\n$tmpK\n$tmpDps310\n$tmpAdj\n$humi\n$humiC\n$pressure\n$humAdj\n$batLevel";
+
+    String influxData =
+        "$pm1\n$pm25\n$pm10\n$pm03ab\n$pm05ab\n$pm1ab\n$pm25ab\n$pm5ab\n$pm10ab\n$tmpC\n$tmpK\n$tmpDps310\n$tmpAdj\n$humi\n$humiC\n$pressure\n$humAdj\n$batLevel";
+
+    // Check if sensor provides AM2320 data, and incorporate them if applicable
+    if (featuresAM2320SensorData()) {
+      var tmpAm2320 = addNestedData("temperature_am2320.c", this.values[SENSOR_TEMP_AM2320], Units.TEMPERATURE_CELSIUS);
+      var humAm2320 = addNestedData("humidity_am2320", this.values[SENSOR_HUMI_AM2320], Units.PERCENTAGE);
+      influxData += "\n$tmpAm2320\n$humAm2320";
+    }
+
+    return influxData;
   }
 
   ///Format data to write many sensorData into influxdb.
@@ -167,4 +176,12 @@ class DataPointModel {
             sensorName: json['deviceName'],
             position: Position(geohash: json['geohash'], provider: PositionProviderUtils.fromString(json['provider']), transport: json['transport']),
             date: json['date']);
+
+  /// AM2320 sensor was added to PAMELA sensors after their deployment, some of
+  /// them don't have this external sensor.
+  /// This allows to check if the current data point contains data produced by
+  /// the AM2320 sensor.
+  bool featuresAM2320SensorData() {
+    return this.values.length >= SENSOR_HUMI_AM2320 + 1;
+  }
 }
---
+++ apolline-flutter/test/data_point_model_test.dart
@@ -0,0 +1,28 @@
+import 'package:apollineflutter/models/data_point_model.dart';
+import 'package:apollineflutter/utils/position/position.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('hasAM2320Sensor', () {
+    test('should return true with complete data', () {
+      String data = "2023_3_30_10_52_53;0;2;3;0;2;3;258;135;76;0.000000;0.000000;0.00;0.00;0;19.06;100649.77;19.95;69.39;4.05;12.75;1.05;19.30;58.50";
+      DataPointModel point = DataPointModel(id: 0, values: data.split(';'), sensorName: "sensorName", position: Position());
+      bool result = point.featuresAM2320SensorData();
+      expect(result, true);
+    });
+
+    test('should return true with data having lots of fields', () {
+      String data = "2023_3_30_10_52_53;0;2;3;0;2;3;258;135;76;0.000000;0.000000;0.00;0.00;0;19.06;100649.77;19.95;69.39;4.05;12.75;1.05;19.30;58.50;92.51:42.42";
+      DataPointModel point = DataPointModel(id: 0, values: data.split(';'), sensorName: "sensorName", position: Position());
+      bool result = point.featuresAM2320SensorData();
+      expect(result, true);
+    });
+
+    test('should return false with data missing fields', () {
+      String data = "2023_3_30_10_52_53;0;2;3;0;2;3;258;135;76;0.000000;0.000000;0.00;0.00;0;19.06;100649.77;19.95;69.39;4.05;12.75;1.05";
+      DataPointModel point = DataPointModel(id: 0, values: data.split(';'), sensorName: "sensorName", position: Position());
+      bool result = point.featuresAM2320SensorData();
+      expect(result, false);
+    });
+  });
+}
