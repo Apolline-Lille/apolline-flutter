99a21184d44faeb992bbd4766f54f0ed7d15d724
Remy Raes <raes.remy@gmail.com>
2023-03-30 11:08:58 +0200

    refactor: make hasAM2320Sensor method public

--- apolline-flutter/lib/models/data_point_model.dart
+++ apolline-flutter/lib/models/data_point_model.dart
@@ -137,7 +137,7 @@ class DataPointModel {
         "$pm1\n$pm25\n$pm10\n$pm03ab\n$pm05ab\n$pm1ab\n$pm25ab\n$pm5ab\n$pm10ab\n$tmpC\n$tmpK\n$tmpDps310\n$tmpAdj\n$humi\n$humiC\n$pressure\n$humAdj\n$batLevel";
 
     // Check if sensor provides AM2320 data, and incorporate them if applicable
-    if (_hasAM2320Sensor()) {
+    if (hasAM2320Sensor()) {
       var tmpAm2320 = addNestedData("temperature_am2320.c", this.values[SENSOR_TEMP_AM2320], Units.TEMPERATURE_CELSIUS);
       var humAm2320 = addNestedData("humidity_am2320", this.values[SENSOR_HUMI_AM2320], Units.PERCENTAGE);
       influxData += "\n$tmpAm2320\n$humAm2320";
@@ -181,7 +181,7 @@ class DataPointModel {
   /// them don't have this external sensor.
   /// This allows to check if the current data point contains data produced by
   /// the AM2320 sensor.
-  bool _hasAM2320Sensor() {
+  bool hasAM2320Sensor() {
     return this.values.length >= SENSOR_HUMI_AM2320 + 1;
   }
 }
